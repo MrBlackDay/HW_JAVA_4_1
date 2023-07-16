36065e55fd1be88e6bf37fb28cdc1ce85828348b
Dima Lis <iselis777888999@gmail.com>
2023-07-16 12:39:53 +0300

    second commit rename variable

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price_ticket) { //расчёт размера бонуса, принимая значение цены билета
+    public int calculate(int priceTicket) { //расчёт размера бонуса, принимая значение цены билета
         int bonus = 20; // Значение количества рублей для одной бонусной мили
-        int miles = price_ticket / bonus; // Расчёт количества бонусных миль
+        int miles = priceTicket / bonus; // Расчёт количества бонусных миль
         return miles;
     }
 }
